419723e5ede39ddd46d265039e4ad961550752d8
Arthur <andradearthurf@gmail.com>
2022-03-08 21:31:20 -0300

    adding user queries

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,32 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = this.repository.findOneOrFail({id: user_id}, {relations: ['games']})
+    // Para eu buscar, também, a tabela games, devo usar o findOneOrFail, já
+    // que games tem uma relação de muito pra muitos com user.
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT * 
+      FROM users 
+      ORDER BY first_name ASC`
+    ); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Tenho que retornar tudo do usuário buscado pelo nome.
+    return this.repository.query(
+      `SELECT *
+      FROM users 
+      WHERE LOWER(first_name) = LOWER($1) 
+      AND LOWER(last_name) = LOWER($2)
+      `, [first_name, last_name] 
+    ); // Complete usando raw query
   }
 }
